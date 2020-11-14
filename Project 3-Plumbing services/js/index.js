16952a64343c0c052f1aadfae9a64beb33aaa72b
JanCesenek <jan.cesenekCZ@gmail.com>
2020-11-14 16:53:35 +0000

    personal website draft

---
+++ Project 3-Plumbing services/js/index.js
@@ -0,0 +1,23 @@
+var chapters = document.getElementsByClassName("nav__link");
+
+for (var i = 0; i < chapters.length; i++) {
+  chapters[i].addEventListener("click", function(){
+  var current = document.getElementsByClassName("focused");
+  current[0].className = current[0].className.replace(" focused", "");
+  this.className += " focused";
+  });
+}
+
+function ShowTextFor(selectedMenuOption) {
+	var menuContentClass = document.getElementsByClassName("main-content__chapter");
+  
+  for(var i = 0; i < menuContentClass.length; i++) {
+    var menuContentId = document.getElementsByClassName("main-content__chapter")[i].id;
+    if(menuContentId == selectedMenuOption){
+      document.getElementById(menuContentId).classList.remove("hidden");
+    } 
+    else{
+      document.getElementById(menuContentId).classList.add("hidden");
+    }
+  }
+}
