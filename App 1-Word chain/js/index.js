2309965589918302cfe44471efdf8d3f4abf893f
JanCesenek <jan.cesenekCZ@gmail.com>
2021-01-28 12:23:20 +0000

    app update

--- App 1-Word chain/js/index.js
+++ App 1-Word chain/js/index.js
@@ -1,33 +1,48 @@
+'use strict';
+
 const listofWords1 = [];
-const listofWords2 = [];
+const listofWords2 = ['carrot'];
 
 const input = document.querySelector('input');
-const wordList1 = document.getElementsByClassName('word-list1');
-const wordList2 = document.getElementsByClassName('word-list2');
-const score1 = document.getElementsByClassName('actual-score1');
-const score2 = document.getElementsByClassName('actual-score2');
-const player1 = document.getElementsByClassName('player1');
-const player2 = document.getElementsByClassName('player2');
-const arrow1 = document.getElementsByClassName('arrow-1');
-const arrow2 = document.getElementsByClassName('arrow-2');
+const wordList1 = document.querySelector('.word-list1');
+const wordList2 = document.querySelector('.word-list2');
+let score1 = 0;
+let score2 = 0;
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+const arrow1 = document.querySelector('.arrow-1');
+const arrow2 = document.querySelector('.arrow-2');
+let activePlayer = 0;
+let wordCon1 = listofWords1[listofWords1.length - 1];
+let wordCon2 = listofWords2[listofWords2.length - 1];
 
 const updateValue = function(value) {
-    if (key === 'Enter') {
-        if (player1.classList.contains('active-player')) {
-            listofWords1.push(value);
-            wordList1.textContent = listofWords1;
-            player1.classList.remove('active-player');
-            player2.classList.add('active-player');
+    if (value.key === 'Enter') {
+        if (activePlayer === 0 ? wordCon2.charAt(wordCon2.length - 1) === value.charAt(0): wordCon1.charAt(wordCon1.length - 1) === value.charAt(0) && (!listofWords1.contains(value) && !listofWords2.contains(value))) {
+            if (player1.classList.contains('active-player')) {
+                listofWords1.push(value);
+                wordList1.textContent += listofWords1[listofWords1.length - 1];
+                player1.classList.remove('active-player');
+                player2.classList.add('active-player');
+                value.length += score1;
+                activePlayer = 1;
+            } else {
+                listofWords2.push(value);
+                wordList2.textContent += listofWords2[listofWords2.length - 1];
+                player2.classList.remove('active-player');
+                player1.classList.add('active-player');
+                value.length += score2;
+                activePlayer = 0;
+            }
+        } else if (listofWords1.contains(value) || listofWords2.contains(value)) {
+            alert('❌ That word has already been used!');
         } else {
-            listofWords2.push(value);
-            wordList2.textContent = listofWords2;
-            player2.classList.remove('active-player');
-            player1.classList.add('active-player');
+            alert('⛔ Invalid word!');
         }
     }
 }
 
-input.addEventListener('input', updateValue); 
+input.addEventListener('keydown', updateValue);
 
 arrow1.addEventListener('click', function() {
     wordList1.classList.toggle('hidden');
